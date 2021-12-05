7f995c9d040f305d2e061682b23f5ac4cc6104c9
Ken Yoshizoe <kenyoshizoe@gmail.com>
2021-12-05 19:09:57 +0900

    add  trackpad

---
+++ keyboards/lily58_x/keymaps/trackpad/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define USE_SERIAL_PD2
+
+#define TAPPING_FORCE_HOLD
---
+++ keyboards/lily58_x/keymaps/trackpad/rules.mk
@@ -0,0 +1,9 @@
+POINTING_DEVICE_ENABLE = yes
+SRC +=  ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
+        analog.c
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
