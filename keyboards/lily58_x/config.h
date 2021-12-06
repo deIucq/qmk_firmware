029e0392550a119d6252770c6cf591b7babf6cc5
Ken Yoshizoe <kenyoshizoe@gmail.com>
2021-12-06 09:57:51 +0900

    change lily58_x settings

--- keyboards/lily58_x/config.h
+++ keyboards/lily58_x/config.h
@@ -47,9 +47,43 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Locking resynchronize hack */
 //#define LOCKING_RESYNC_ENABLE
 
-/* ws2812 RGB LED */
+/* LED */
 #define RGB_DI_PIN D3
-#define RGBLED_NUM 12    // Number of LEDs
+#define RGBLED_NUM 58
+#define RGBLED_SPLIT {29, 29}
+#define RGBLIGHT_MAX_LAYERS 8
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 10
+#define RGBLIGHT_VAL_STEP 20
+#define RGBLIGHT_LIMIT_VAL 120
+//#define RGBLIGHT_ANIMATIONS
+//#define RGBLIGHT_SLEEP
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_MAX_LAYERS 8
+#define LED_LAYOUT( \
+	L00, L01, L02, L03, L04, L05,           R00, R01, R02, R03, R04, R05,  \
+	L10, L11, L12, L13, L14, L15,           R10, R11, R12, R13, R14, R15,  \
+	L20, L21, L22, L23, L24, L25,           R20, R21, R22, R23, R24, R25,  \
+	L30, L31, L32, L33, L34, L35, L45, R40, R30, R31, R32, R33, R34, R35, \
+	               L41, L42, L43, L44, R41, R42, R43, R44  \
+) { \
+    L05, L04, L03, L02, L01, L00, \
+    L10, L11, L12, L13, L14, L15, \
+    L25, L24, L23, L22, L21, L20, \
+    L30, L31, L32, L33, L34, L35, \
+    L45, L44, L43, L42, L41, \
+    R00, R01, R02, R03, R04, R05, \
+    R15, R14, R13, R12, R11, R10, \
+    R20, R21, R22, R23, R24, R25, \
+    R35, R34, R33, R32, R31, R30, \
+    R40, R41, R42, R43, R44 \
+}
+#define RGBLIGHT_LED_MAP LED_LAYOUT( \
+   0,  1,  2,  3,  4,  5,         34, 33, 32, 31, 30, 29, \
+   6,  7,  8,  9, 10, 11,         40, 39, 38, 37, 36, 35, \
+  12, 13, 14, 15, 16, 17,         46, 45, 44, 43, 42, 41, \
+  18, 19, 20, 21, 22, 23, 24, 53, 52, 51, 50, 49, 48, 47, \
+              25, 26, 27, 28, 57, 56, 55, 54)
 
 /*
  * Feature disable options
--- keyboards/lily58_x/rules.mk
+++ keyboards/lily58_x/rules.mk
@@ -9,16 +9,19 @@ BOOTLOADER = caterina
 #   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
+COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306    # OLED display
+OLED_ENABLE = yes           # OLED display
+OLED_DRIVER = SSD1306       # Type of OLED display
 SPLIT_KEYBOARD = yes
+
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 EXTRAFLAGS += -flto
