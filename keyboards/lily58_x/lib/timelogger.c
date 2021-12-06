28bf547a72a2c1e6aa6112e8e2ad4b322b3dda93
Ken Yoshizoe <kenyoshizoe@gmail.com>
2021-12-06 09:57:58 +0900

    change timelogger.c

--- keyboards/lily58_x/lib/timelogger.c
+++ keyboards/lily58_x/lib/timelogger.c
@@ -4,12 +4,15 @@
 
 char timelog_str[24] = {};
 int last_time = 0;
-int elapsed_time = 0;
+uint32_t elapsed_time = 0;
 
 void set_timelog(void) {
-  elapsed_time = timer_elapsed(last_time);
-  last_time = timer_read();
-  snprintf(timelog_str, sizeof(timelog_str), "lt:%5d, et:%5d", last_time, elapsed_time);
+  elapsed_time = timer_elapsed32(last_time);
+  int d = elapsed_time/1000/60/60/24;
+  int h = elapsed_time/1000/60/60%24;
+  int m = elapsed_time/1000/60%60;
+  int s = elapsed_time/1000%60;
+  snprintf(timelog_str, sizeof(timelog_str), "%ddays %02d:%02d:%02d012345",d,h,m,s);
 }
 
 const char *read_timelog(void) {
