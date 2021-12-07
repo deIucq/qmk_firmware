57029482df3b69ff024b5c41a8b694d37bc8ed4f
Ken Yoshizoe <kenyoshizoe@gmail.com>
2021-12-07 23:11:13 +0900

    change layer_state_reader

--- keyboards/lily58_x/lib/layer_state_reader.c
+++ keyboards/lily58_x/lib/layer_state_reader.c
@@ -6,7 +6,8 @@
 #define L_BASE 0
 #define L_LOWER (1 << 1)
 #define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
+#define L_RGB (1 << 3)
+#define L_GAMING (1 << 4)
 #define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
 
 char layer_state_str[24];
@@ -23,13 +24,13 @@ const char *read_layer_state(void) {
   case L_LOWER:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
     break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+  case L_RGB:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: RGB");
+  case L_GAMING:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: GAMING");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
   }
-
   return layer_state_str;
 }
