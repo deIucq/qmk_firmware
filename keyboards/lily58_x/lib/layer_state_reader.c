95e2385095ba63e055298c10b609ebffff78b0ed
Ken Yoshizoe <kenyoshizoe@gmail.com>
2021-12-08 13:47:59 +0900

    fix layer statereader.c

--- keyboards/lily58_x/lib/layer_state_reader.c
+++ keyboards/lily58_x/lib/layer_state_reader.c
@@ -3,19 +3,18 @@
 #include <stdio.h>
 #include "lily58_x.h"
 
-#define L_BASE 0
+#define L_DEFAULT 0
 #define L_LOWER (1 << 1)
 #define L_RAISE (1 << 2)
-#define L_RGB (1 << 3)
-#define L_GAMING (1 << 4)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+#define L_GAMING (1 << 3)
+#define L_RGB (1 << 4)
 
 char layer_state_str[24];
 
 const char *read_layer_state(void) {
   switch (layer_state)
   {
-  case L_BASE:
+  case L_DEFAULT:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
     break;
   case L_RAISE:
@@ -24,10 +23,11 @@ const char *read_layer_state(void) {
   case L_LOWER:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
     break;
-  case L_RGB:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: RGB");
   case L_GAMING:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: GAMING");
+    break;
+  case L_RGB:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: RGB");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
