bf0181275b4fd5cc7e99f5c520243dbe1a2122fe
Ken Yoshizoe <kenyoshizoe@gmail.com>
2021-12-10 21:31:24 +0900

    add trackpad keymap

---
+++ keyboards/lily58_x/keymaps/trackpad_master/rules.mk
@@ -0,0 +1,3 @@
+POINTING_DEVICE_ENABLE = yes
+
+SRC +=  ./lib/trackpad.c \
---
+++ keyboards/lily58_x/lib/trackpad.c
@@ -0,0 +1,67 @@
+#include "lily58_x.h"
+#include <math.h>
+
+#define JOYSTICK_X_PIN 3
+#define JOYSTICK_Y_PIN 6
+#define X_INVERT true
+#define Y_INVERT true
+#define DEADZONE 30
+#define GAIN 0.05
+
+int trackpad_origin[2] = {512, 512};
+
+void trackpad_init(void){
+    BMPAPI->adc.config_vcc_channel(JOYSTICK_X_PIN, 3300, 0);
+    trackpad_origin[0] = BMPAPI->app.get_vcc_mv();
+    BMPAPI->adc.config_vcc_channel(JOYSTICK_Y_PIN, 3300, 0);
+    trackpad_origin[1] = BMPAPI->app.get_vcc_mv();
+}
+
+int get_r(void){
+    BMPAPI->adc.config_vcc_channel(JOYSTICK_X_PIN, 1200, 700);
+    int xSensVal = (trackpad_origin[0] - BMPAPI->app.get_vcc_mv()) * (X_INVERT ? -1 : 1);
+    BMPAPI->adc.config_vcc_channel(JOYSTICK_Y_PIN, 1200, 700);
+    int ySensVal = (trackpad_origin[1] - BMPAPI->app.get_vcc_mv()) * (Y_INVERT ? -1 : 1);
+    int r = sqrt(pow(xSensVal,2.0) + pow(ySensVal,2.0));
+    return r;
+}
+
+int get_theta(void){
+    BMPAPI->adc.config_vcc_channel(JOYSTICK_X_PIN, 1200, 700);
+    int xSensVal = (trackpad_origin[0] - BMPAPI->app.get_vcc_mv()) * (X_INVERT ? -1 : 1);
+    BMPAPI->adc.config_vcc_channel(JOYSTICK_Y_PIN, 1200, 700);
+    int ySensVal = (trackpad_origin[1] - BMPAPI->app.get_vcc_mv()) * (Y_INVERT ? -1 : 1);
+
+    int r = sqrt(pow(xSensVal,2.0) + pow(ySensVal,2.0));
+    double theta;
+    if (ySensVal > 0)
+        theta = acos((double)xSensVal / (double)r);
+    else
+        theta = - acos((double)xSensVal / (double)r) + 2 * M_PI;
+    return (theta / 2.0 / M_PI) * 360;
+}
+
+report_mouse_t get_trackpad_report(void){
+    report_mouse_t mouseReport;
+    BMPAPI->adc.config_vcc_channel(JOYSTICK_X_PIN, 1200, 700);
+    int xSensVal = (trackpad_origin[0] - BMPAPI->app.get_vcc_mv()) * (X_INVERT ? -1 : 1);
+    BMPAPI->adc.config_vcc_channel(JOYSTICK_Y_PIN, 1200, 700);
+    int ySensVal = (trackpad_origin[1] - BMPAPI->app.get_vcc_mv()) * (Y_INVERT ? -1 : 1);
+    int r = sqrt(pow(xSensVal,2.0) + pow(ySensVal,2.0));
+    double theta;
+    if (ySensVal > 0)
+        theta = acos((double)xSensVal / (double)r);
+    else
+        theta = - acos((double)xSensVal / (double)r) + 2 * M_PI;
+    if(r > DEADZONE){
+        mouseReport.x = (r - DEADZONE) * cos(theta) * GAIN;
+        mouseReport.y = (r - DEADZONE) * sin(theta) * GAIN;
+    }else{
+        mouseReport.x = 0;
+        mouseReport.y = 0;
+    }
+    mouseReport.v = 0;
+    mouseReport.h = 0;
+    mouseReport.buttons = 0;
+    return mouseReport;
+}
