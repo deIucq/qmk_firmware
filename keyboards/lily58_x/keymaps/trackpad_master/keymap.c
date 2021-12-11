be0ca706d0353179cbc04d9eb7fecf66cac40f68
Ken Yoshizoe <kenyoshizoe@gmail.com>
2021-12-11 11:55:15 +0900

    delete unused functions

--- keyboards/lily58_x/keymaps/trackpad_master/keymap.c
+++ keyboards/lily58_x/keymaps/trackpad_master/keymap.c
@@ -47,49 +47,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
-/*
-//SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master())
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  return rotation;
-}
-// When you add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
-
-int get_r(void);
-int get_theta(void);
-
-bool oled_task_user(void) {
-  if (is_keyboard_master()) {
-    // If you want to change the display of OLED, you need to change here
-    oled_write_ln(read_layer_state(), false);
-    //oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
-    char origin[24];
-    sprintf(origin, "R:%d Theta:%d", get_r(), get_theta());
-    oled_write_ln(origin, false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    //oled_write_ln(read_host_led_state(), false);
-    //oled_write_ln(read_timelog(), false);
-  } else {
-    oled_write(read_logo(), false);
-  }
-    return false;
-}
-*/
-
 void trackpad_init(void);
 report_mouse_t get_trackpad_report(void);
 
